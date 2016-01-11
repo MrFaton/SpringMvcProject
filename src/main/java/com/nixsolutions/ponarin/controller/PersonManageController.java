077f28600134a759c4e86f1ae522e364ce7db5ce
ponarin <ponarin@ponarin-lx.nixsolutions.com>
2016-01-11 18:47:20 +0200

    drop unused import

--- src/main/java/com/nixsolutions/ponarin/controller/PersonManageController.java
+++ src/main/java/com/nixsolutions/ponarin/controller/PersonManageController.java
@@ -1,6 +1,5 @@
 package com.nixsolutions.ponarin.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
